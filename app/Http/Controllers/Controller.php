83ff4f0f7399157a2386209c53854db8cc7ae730
Amershadow <amersohailroot@gmail.com>
2022-06-02 13:18:10 +0530

    Example of getting list of all unused packages present in composer.json

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function FunctionName()
+    {
+        $x=new GuzzleException();
+
+    }
+}
